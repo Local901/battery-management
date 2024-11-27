14cae63a9245145ebe370def063a54127bdd4544
Local901 <thomas.vermeulen@live.nl>
2024-11-27 20:46:25 +0100

    fix

--- src/rootfs/src/customTime.py
+++ src/rootfs/src/customTime.py
@@ -21,8 +21,6 @@ class Time():
         if (butAfter is not None) and self.before(butAfter) and Time.isBeforeNow(butAfter):
             return False
 
-        currentTime = datetime.now()
-        print(f"{timezone.utcoffset}")
         if self.hour < currentTime.hour:
             return True
         elif self.hour == currentTime.hour and self.minutes <= currentTime.minute:
